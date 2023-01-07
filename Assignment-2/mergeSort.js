caa42cb189f34290a5573cd6860c3cbe403b6b9c
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-01-07 16:56:07 +0700

    Create merge function to merge two already-sorted list

--- Assignment-2/mergeSort.js
+++ Assignment-2/mergeSort.js
@@ -1,6 +1,7 @@
-arr = [1,2,3,4,5]
+arr = [1, 9, 17]
+anotherArr = [8, 15]
 
-function splitTest(array) {
+function split(array) {
   const n = array.length;
   console.log(n);
 
@@ -14,4 +15,29 @@ function splitTest(array) {
   console.log(secondHalf);
 }
 
-splitTest(arr);
+function merge(array1, array2) {
+  const len1 = array1.length;
+  const len2 = array2.length;
+  const result = [];
+  
+  let i = 0, j=0;
+
+  while (i < len1 && j < len2) {
+    array1[i] < array2[i] 
+    ? result.push(array1[i++]) 
+    : result.push(array2[j++]);
+  }
+  
+  for (; i < len1; i++) {
+    result.push(array1[i])
+  }
+  for (; j < len2; j++) {
+    result.push(array2[j])
+  }
+
+  return result;
+}
+
+// split(arr);
+
+console.log(merge(arr, anotherArr))
