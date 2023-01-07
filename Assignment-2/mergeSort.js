0a804f76c3c1c33c4274a06a60468edcad425d44
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-01-07 15:14:55 +0700

    Test splitting an array into first half and second half

--- Assignment-2/mergeSort.js
+++ Assignment-2/mergeSort.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+arr = [1,2,3,4,5]
+
+function splitTest(array) {
+  const n = array.length;
+  console.log(n);
+
+  const halfLength = Math.ceil(n/2);
+  console.log(halfLength)
+
+  const firstHalf = array.slice(0, halfLength);
+  console.log(firstHalf)
+
+  const secondHalf = array.slice(halfLength-1, -1);
+  console.log(secondHalf);
+}
+
+splitTest(arr);
